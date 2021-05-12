67ca05e688eadc88bf5f9b7390fbe30873a06a0b
nacho furtado <nacho-furtado@live.com>
2021-05-11 21:57:53 -0300

    nuevas vistas

---
+++ app/src/main/java/com/example/tipymovies/AgregarPregunta.java
@@ -0,0 +1,14 @@
+package com.example.tipymovies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AgregarPregunta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_pregunta);
+    }
+}
