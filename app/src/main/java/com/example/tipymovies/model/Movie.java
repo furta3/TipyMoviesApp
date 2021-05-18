57a31776df973931f06ac378e3ad5fcf827588f8
nacho furtado <nacho-furtado@live.com>
2021-05-17 21:46:32 -0300

    Empezando con lo de listar peliculas, no se para que sirve el Adaptador

---
+++ app/src/main/java/com/example/tipymovies/model/Movie.java
@@ -0,0 +1,54 @@
+package com.example.tipymovies.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Movie {
+    @SerializedName("Title")
+    private String title;
+    @SerializedName("Year")
+    private String year;
+    @SerializedName("imdbID")
+    private String imdbID;
+    @SerializedName("Poster")
+    private String poster;
+
+    public Movie(String title, String year, String imdbID, String poster){
+        this.title=title;
+        this.year=year;
+        this.imdbID=imdbID;
+        this.poster=poster;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getImdbID() {
+        return imdbID;
+    }
+
+    public void setImdbID(String imdbID) {
+        this.imdbID = imdbID;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+}
