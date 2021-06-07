89d44f40a14053ba0b812ecb468c89b46f0fb752
nacho furtado <nacho-furtado@live.com>
2021-06-07 18:33:03 -0300

    no c

--- app/src/main/java/com/example/tipymovies/Config.java
+++ app/src/main/java/com/example/tipymovies/Config.java
@@ -4,5 +4,5 @@ public class Config {
     public static String PACKAGE_NAME = "com.example.tipymovies";
     public static final String API_BASE_URL = "http://www.omdbapi.com/";
     public final static String API_KEY = "169e719d";
-    public static final String TTPY_MOVIES_URL = "http://f95972e5e3b7.ngrok.io/"; //Ejecutar NGROK y escribir "ngrok http 8000", cp de la url
+    public static final String TTPY_MOVIES_URL = "http://f95972e5e3b7.ngrok.io/"; //si usas el emulador: http://localhost:8000 sino: Ejecutar NGROK y escribir "ngrok http 8000", cp de la url
 }
